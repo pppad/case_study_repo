58ae27a5ca85993d7ae34b6fdd628b8dfd17d047
Pantelides.PP@gmail.com <Pantelides.PP@gmail.com>
2026-05-01 15:09:24 +0000

    feat: implement defensive staging logic with COALESCE

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,8 +1,8 @@
 SELECT 
     -- assign the correct datatypes and needed-only columns
-    SAFE_CAST(b AS int64) AS customer_id,
-    c AS customer_type,
-    d AS current_address_country,
+    COALESCE(SAFE_CAST(b AS int64), -1) AS customer_id, -- defensive coding, dummy value to flag any null customer ids 
+    COALESCE(c, 'Unknown') AS customer_type, -- defensive coding, for better audit trail
+    COALESCE(d, 'Unknown') AS current_address_country, -- defensive coding, for better audit trail
     SAFE_CAST(e AS DATE) AS customer_since_date
 FROM 
     {{ ref('Customer') }}  -- the raw data source
--- models/staging/stg_transactions.sql
+++ models/staging/stg_transactions.sql
@@ -1,9 +1,9 @@
 SELECT  
   -- assign the correct datatypes and needed-only columns
-    SAFE_CAST(b AS int64) AS transaction_id,
-    SAFE_CAST(c AS int64) AS customer_id,
-    SAFE_CAST(d AS float64) AS amount_GBP,
-    e AS currency_route,
+    COALESCE(SAFE_CAST(b AS int64), -1) AS transaction_id, -- defensive coding, dummy value to flag any null transaction ids
+    COALESCE(SAFE_CAST(c AS int64), -1) AS customer_id, -- defensive coding, dummy value to flag any null customer ids
+    COALESCE(SAFE_CAST(d AS float64), 0) AS amount_GBP, -- sum ignores nulls but safer that way
+    COALESCE(e, "Unknown --> Unknown") AS currency_route, -- coalesce for all to create a clear audit trail for missing data
     SAFE_CAST(f AS DATE) AS transaction_date
 FROM 
     {{ ref('Transactions') }}  -- the raw data source
